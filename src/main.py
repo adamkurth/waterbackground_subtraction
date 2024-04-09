d5cb89a5040f6e69a36ba949e3f0da1855e778bb
Adam Kurth <amkurth@asu.edu>
2024-04-09 14:55:48 -0700

    revised implemetation with both coordinate menu options

--- src/main.py
+++ src/main.py
@@ -19,8 +19,8 @@ low_image = dh.load_h5_image(image_name='9_18_23_low_intensity_3e7keV-1.h5')
 ip_high = imageprocessor.ImageProcessor(high_image, 100)
 ip_low = imageprocessor.ImageProcessor(low_image, 100)
 
-# high_stream, high_intensity, _ = dh.load_stream_data('test_high.stream')
-# low_stream, low_intensity, _  = dh.load_stream_data('test_low.stream')
+high_stream, high_intensity, _ = dh.load_stream_data('test_high.stream')
+low_stream, low_intensity, _  = dh.load_stream_data('test_low.stream')
 
 # using skimage: peak_local_max
 high_coordinates = ip_high.find_peaks()
@@ -29,14 +29,13 @@ low_coordinates = ip_low.find_peaks()
 # # visualize
 # ip_high.visualize_peaks()
 # ip_low.visualize_peaks()
-
 # ip_high.visualize_image_3d(high_coordinates, ip_high.threshold)
 # ip_low.visualize_image_3d(low_coordinates, ip_low.threshold)
 
 # default background subtraction demo
 b = background.BackgroundSubtraction()
-# for r in b.radii:
-#     b.coordinate_menu(r)
+for r in b.radii:
+    b.coordinate_menu(r)
 
 data = b.coordinate_menu_streamlined()
 print(data)
@@ -45,9 +44,7 @@ print(data)
 # functions.display_peaks_3d(b.loaded_image, b.coordinates, b.p.threshold_value)
 # functions.display_peaks_3d_beamstop(b.loaded_image, b.p.threshold_value)
 
-# stream/overwrite adaptation demo
-# USE DH
-
+# only uncomment if want to overwrite stream
 # dh.overwrite(high_stream)
 
 
