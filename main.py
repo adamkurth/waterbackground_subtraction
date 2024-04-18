000c30dab10bfcde9c1e49c0afc74206444571b2
Adam Kurth <k33go75@gmail.com>
2024-04-18 16:18:09 -0700

    update

--- main.py
+++ main.py
@@ -2,13 +2,24 @@
 import os
 from pathlib import Path
 from finder import *
+import numpy as np
 
-base = Path('../../../../../').resolve() 
-images_dir = Path(base, 'images').resolve()
+base = Path('../../../../../').resolve() # should be vscode/CXFEL
+images_dir = Path(base, 'images').resolve() # should be vscode/CXFEL/images
 print(f"Base directory: {base}")
 print(f'Images directory: {images_dir} \n') 
 
 files = [str(f) for f in images_dir.glob('*.h5') if f.name.startswith('img')]
+
+# tesing peaks in cxls_hitfinder
+peaks_dir = Path(base, 'cxls_hitfinder', 'images', 'peaks', '01').resolve()
+print(f'Peaks directory: {peaks_dir} \n')
+
+peaks_image = [f for f in peaks_dir.glob('*.h5') if f.name.startswith('img')][0] 
+peaks_image, _ = functions.load_h5(peaks_image)
+print(f'Test image shape: {peaks_image.shape}')
+np.save('/Users/adamkurth/Documents/vscode/CXFEL/cxls_hitfinder/cnn/src/pkg/waterbackground_subtraction/peaks.npy', peaks_image)
+
 
 # test_image, image_path = functions.load_h5(images_dir) #random 
 
@@ -30,8 +41,17 @@ b = background.BackgroundSubtraction()
 # streamlined method
 # dataframe = b.coordinate_menu_streamlined()
 
-dataframe = b.main(files)
-print(dataframe)
+
+# test_image, image_path = functions.load_h5(files[0])
+# np.save('/Users/adamkurth/Documents/vscode/CXFEL/cxls_hitfinder/cnn/src/pkg/waterbackground_subtraction/test_image.npy', test_image)
+
+# ip = imageprocessor.ImageProcessor(test_image, 100)
+
+# print(ip.coordinates)
+
+# # 
+# dataframe = b.main(files)
+# print(dataframe)
 
 # view waterbackground 
 # functions.display_peaks_3d(b.loaded_image, b.coordinates, b.p.threshold_value)
